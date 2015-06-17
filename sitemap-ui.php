d31197444fe13dd7e6e1caeee0c9288797aa446d
MikeIrvine <MikeIrvine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 02:22:44 +0000

    UI Updated
    git-svn-id: https://plugins.svn.wordpress.org/google-sitemap-xml/trunk@1182275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sitemap-ui.php
+++ sitemap-ui.php
@@ -687,40 +687,9 @@ HTML;
 
 
 					<?php if(!$snl): ?>
-							<?php $this->HtmlPrintBoxHeader('sm_pnres',__('About this Plugin:','sitemap'),true); ?>
-								<a class="sm_button sm_pluginHome"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Plugin Homepage','sitemap'); ?></a>
-								<a class="sm_button sm_pluginHome"    href="<?php echo $this->sg->GetRedirectLink('sitemap-feedback'); ?>"><?php _e('Suggest a Feature','sitemap'); ?></a>
-								<a class="sm_button sm_pluginHelp"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Help / FAQ','sitemap'); ?></a>
-								<a class="sm_button sm_pluginList"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Notify List','sitemap'); ?></a>
-								<a class="sm_button sm_pluginSupport" href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Support Forum','sitemap'); ?></a>
-								<a class="sm_button sm_pluginBugs"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Report a Bug','sitemap'); ?></a>
-
-								<a class="sm_button sm_donatePayPal"  href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Donate with PayPal','sitemap'); ?></a>
-								<a class="sm_button sm_donateAmazon"  href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('My Amazon Wish List','sitemap'); ?></a>
-								<?php if(__('translator_name','sitemap')!='translator_name') {?><a class="sm_button sm_pluginSupport" href="<?php _e('translator_url','sitemap'); ?>"><?php _e('translator_name','sitemap'); ?></a><?php } ?>
-							<?php $this->HtmlPrintBoxFooter(true); ?>
-
-							<?php $this->HtmlPrintBoxHeader('sm_smres',__('Sitemap Resources:','sitemap'),true); ?>
-								<a class="sm_button sm_resGoogle"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Webmaster Tools','sitemap'); ?></a>
-								<a class="sm_button sm_resGoogle"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Webmaster Blog','sitemap'); ?></a>
-
-								<a class="sm_button sm_resYahoo"     href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Search Blog','sitemap'); ?></a>
-
-								<a class="sm_button sm_resBing"      href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Webmaster Tools','sitemap'); ?></a>
-								<a class="sm_button sm_resBing"      href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Webmaster Center Blog','sitemap'); ?></a>
-								<br />
-								<a class="sm_button sm_resGoogle"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Sitemaps Protocol','sitemap'); ?></a>
-								<a class="sm_button sm_resGoogle"    href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('Official Sitemaps FAQ','sitemap'); ?></a>
-								<a class="sm_button sm_pluginHome"   href="<?php echo $this->sg->GetRedirectLink('seo-content'); ?>"><?php _e('My Sitemaps FAQ','sitemap'); ?></a>
-							<?php $this->HtmlPrintBoxFooter(true); ?>
-
-							<?php $this->HtmlPrintBoxHeader('dm_donations',__('Recent Donations:','sitemap'),true); ?>
-								<?php if($this->sg->GetOption('i_hide_donors')!==true) { ?>
-									<iframe border="0" frameborder="0" scrolling="no" allowtransparency="yes" style="width:100%; height:80px;" src="<?php echo $this->sg->GetRedirectLink('sitemap-donorlist'); ?>">
-									<?php _e('List of the donors','sitemap'); ?>
-									</iframe><br />
-									<a href="<?php echo $this->sg->GetBackLink() . "&amp;sm_hidedonors=true"; ?>"><small><?php _e('Hide this list','sitemap'); ?></small></a><br /><br />
-								<?php } ?>
+							
+							<?php $this->HtmlPrintBoxHeader('dm_donations',__('Donate:','sitemap'),true); ?>
+								
 								<a style="float:left; margin-right:5px; border:none;" href="javascript:document.getElementById('sm_donate_form').submit();"><img style="vertical-align:middle; border:none; margin-top:2px;" src="<?php echo $this->sg->GetPluginUrl(); ?>img/icon-donate.gif" border="0" alt="PayPal" title="Help me to continue support of this plugin :)" /></a>
 								<span><small><?php _e('Thanks for your support!','sitemap'); ?></small></span>
 								<div style="clear:left; height:1px;"></div>
@@ -1309,4 +1278,3 @@ HTML;
 		<?php
 	}
 }
-
